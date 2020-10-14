cf1c99ab965f7ed0560a142c9327354e83b3b99a
frafusaro <fra.fusaro@gmail.com>
2020-10-14 07:19:47 +0200

    moved outlier detection and added ETC

--- project1_ffu/main.py
+++ project1_ffu/main.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression, Lasso, Ridge
 from sklearn.feature_selection import RFE, RFECV
 from sklearn.ensemble import IsolationForest
+from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.decomposition import PCA
 from sklearn import preprocessing
 import matplotlib.pyplot as plt
@@ -10,7 +11,7 @@ import numpy as np
 import logging
 import os.path
 
-def feature_reduction(df_x,cvmin):
+def feature_reduction(df_x,cvmin,cvmax):
 #identify columns which contains constant values and some noise.
 #use the coefficient of variation CV as metric (CV = sigma/mean).
 #drop columns in which CV < cvmin.
@@ -25,7 +26,7 @@ def feature_reduction(df_x,cvmin):
          n = n+1
      else: 
          cv = df_x[column].astype("float").std()/mean
-         if cv < cvmin:
+         if cv < cvmin or cv > cvmax:
              dropped_cols_cv.append(column)
              n = n+1
  print(f"dropped {n} out of {n_tot}")
@@ -53,12 +54,52 @@ def remove_nan(df_x,tool):
              df_x[column].replace(np.nan, mean, inplace=True)
  return(df_x)
 
+def remove_outlier(df_x, df_y,cont_lim):
+#use Isolation Forest to indentify outliers on the selected features
+    #iso_Y_arr, iso_X_arr, df_colnames = df_to_array(df)
+    iso_Y_arr = df_y.to_numpy()
+    iso_X_arr = df_x.to_numpy()
+
+
+    len_Y_outl = len(iso_Y_arr)
+    
+    iso = IsolationForest(contamination=cont_lim)
+    yhat = iso.fit_predict(df_x)
+    mask = yhat != -1
+    iso_X_arr, iso_Y_arr = iso_X_arr[mask, :], iso_Y_arr[mask]
+
+    len_Y_inl = len_Y_outl-sum(mask)
+    print(f"Outliers removal:")
+    print(f"Removing {len_Y_inl} outliers out of {len_Y_outl} datapoints.")
+    
+    #numrows = len(iso_X_arr)    
+    #numcols = len(iso_X_arr[0])
+    #iso_arr = np.random.rand(numrows,numcols+1)
+    #iso_arr[:,0]=iso_Y_arr
+    df_y_iso = pd.DataFrame(data=iso_Y_arr, columns=df_y.columns.tolist())
+    df_x_iso = pd.DataFrame(data=iso_X_arr, columns=df_x.columns.tolist())
+    return(df_x_iso, df_y_iso)
+
+def extract_top_ETC(df_x,df_y,n_top,rnd_state):
+# check feature importance with Extra Trees Classifier
+# https://towardsdatascience.com/feature-selection-techniques-in-machine-learning-with-python-f24e7da3f36e 
+  model = ExtraTreesClassifier(random_state=rnd_state)
+  print(df_x.head(3))
+  print(df_y.head(3))
+  model.fit(df_x,df_y)
+  #print(model.feature_importances_)
+  feat_importances = pd.Series(model.feature_importances_, index=df_x.columns)
+  #print(feat_importances.nlargest(rnd_state).index)
+  df_x_ETC = df_x[feat_importances.nlargest(rnd_state).index]
+  print(df_x_ETC.head(5))
+  return(df_x_ETC,df_y)
+
 def extract_y_correlated(df_x,df_y):
 #calculate correlation with y and extract most correlated ones    
  
  #merge dataframes
  df = pd.concat([df_y,df_x],axis=1)
- df.drop(["id"],axis=1,inplace=True)
+ #df.drop(["id"],axis=1,inplace=True) #alreasy taken care of.
  
  #build the correlation matrix
  p = df.corr()
@@ -74,13 +115,11 @@ def extract_y_correlated(df_x,df_y):
  #features in top and bottom category
  p_y_033_067 = p_y[p_y["y-binned"]=="[0.33-0.67]"]
  p_y_n67_033 = p_y[p_y["y-binned"]=="neg[0.67-0.33]"]
- #l1=["y"]
- #l1.append(p_y_n67_033.index.tolist())
- l1=["y"]+p_y_033_067.index.tolist()+p_y_n67_033.index.tolist()
- print(l1)
- df2 = df[l1]
- print(f"columns kept after correlation check with y: {len(l1)}")
- return(df2)
+ l1=p_y_033_067.index.tolist()+p_y_n67_033.index.tolist()
+ df_x_corr = df_x[l1]
+ print(f"no. of columns kept after correlation check with y: {len(l1)}")
+ print(f"columns kept after correlation check with y:\n {df_x_corr.columns.tolist()}")
+ return(df_x,df_y)
 
 def drop_x_correlated(df,lb):
 #look at correlation between features
@@ -115,31 +154,12 @@ def drop_x_correlated(df,lb):
 
  #drop values from dataframe
  df.drop(lc_unique,axis=1,inplace=True)
+ print(f"These columns are retained:\n{df.columns.tolist()}")
+
+ compare_to_bench(df.columns.tolist())
  return(df)
 
-def remove_outlier(df,cont_lim):
-#use Isolation Forest to indentify outliers on the selected features
-    iso_Y_arr, iso_X_arr, df_colnames = df_to_array(df)
-    len_Y_outl = len(iso_Y_arr)
-    
-    iso = IsolationForest(contamination=cont_lim)
-    yhat = iso.fit_predict(iso_X_arr)
-    mask = yhat != -1
-    iso_X_arr, iso_Y_arr = iso_X_arr[mask, :], iso_Y_arr[mask]
-
-    len_Y_inl = len_Y_outl-sum(mask)
-    print(f"Outliers removal:")
-    print(f"Removing {len_Y_inl} outliers out of {len_Y_outl} datapoints.")
-    
-    numrows = len(iso_X_arr)    
-    numcols = len(iso_X_arr[0])
-    iso_arr = np.random.rand(numrows,numcols+1)
-    iso_arr[:,0]=iso_Y_arr
-    iso_arr[:,1:(numcols+1)]=iso_X_arr
-    df_iso = pd.DataFrame(data=iso_arr, columns=df_colnames)
-    return(df_iso)
-
-def apply_pca(df, n_comp):
+def apply_pca(df_x, df_y, n_comp):
     X_arr, Y_arr = normalize(df)
 
     pca = PCA(n_components=2)
@@ -148,19 +168,37 @@ def apply_pca(df, n_comp):
     print(f"PC 1 with scaling:\n {pca.components_[0]}")
     return(X_pca, Y_arr)
 
-def normalize(df):
-    Y_arr, X_arr, df_colnames = df_to_array(df)
+def normalize(df_x):
+    Y_arr, X_arr, df_colnames = df_to_array(df_x)
 
     min_max_scaler = preprocessing.MinMaxScaler()
     X_arr_scaled= min_max_scaler.fit_transform(X_arr)
+    df_x = pd.DataFrame(data=X_arr_scaled,columns=df_colnames)
     return(X_arr_scaled, Y_arr)
 
-def df_to_array(df):
-    Y_arr = df["y"].to_numpy()
-    df_x = df.drop(["y"],axis=1)
+def df_to_array(df_x):
+    Y_arr = df_y.to_numpy()
     X_arr = df_x.to_numpy()
-    df_colnames = df.columns.tolist()
+    df_x_colnames = df_x.columns.tolist()
     return(Y_arr,X_arr,df_colnames)
+
+def compare_to_bench(cand_list):
+  bench = ["x85", "x302", "x761", "x687", "x507", "x685", "x785", "x540", "x155", "x482", "x356", "x184", "x35", "x546", "x745", "x579", "x344", "x783", "x198", "x476"]
+  lc_found = []
+  lc_not_found =[]
+  lc_not_bench = []
+  for col in bench:
+    if col in cand_list:
+      lc_found.append(col)
+    else:
+      lc_not_found.append(col)
+
+  for col in cand_list:
+      if col not in bench:
+        lc_not_bench.append(col)
+  print(f"These bench columns were not found:\n{lc_not_found}")
+  print(f"These bench columns were found:\n{lc_found}")
+  print(f"These columns are not in bech:\n{lc_not_bench}")
 
 def run(run_cfg, env_cfg):
 #main script
@@ -174,29 +212,38 @@ def run(run_cfg, env_cfg):
     fname = 'X_train.csv'
     fpath = os.path.join(path,fname)
     df_x = pd.read_csv(fpath)
+    df_x.drop(["id"],axis=1,inplace=True)
+
 
     fname = 'y_train.csv'
     fpath = os.path.join(path,fname)
     df_y = pd.read_csv(fpath)
+    df_y.drop(["id"],axis=1,inplace=True)
+
+    print(df_x.head(3))
+    print(df_y.head(3))
 
     #remove features by coefficient of variance
     if run_cfg['preprocessing/feature_removal']:
-     df_x = feature_reduction(df_x,run_cfg['preprocessing/feature_removal_cv_min'])
+     df_x = feature_reduction(df_x,run_cfg['preprocessing/feature_removal_cv_min'],run_cfg['preprocessing/feature_removal_cv_max'])
 
     #remove nan
     df_x = remove_nan(df_x,run_cfg['preprocessing/nan'])
 
+    #remove outliers
+    if run_cfg['preprocessing/outlier_removal']:
+     df_x, df_y = remove_outlier(df_x,df_y,run_cfg['preprocessing/outlier_cont_lim'])
+
+    if run_cfg['preprocessing/extract_top_ETC']:
+     df_x, df_y = extract_top_ETC(df_x,df_y,run_cfg['preprocessing/ETC_n_top'],run_cfg['preprocessing/ETC_rs'])
+    
     #extract features by y correlation
     if run_cfg['preprocessing/y_corr']:
      df_yx = extract_y_correlated(df_x,df_y)
 
     #remove features by x correlation
     if run_cfg['preprocessing/x_corr']:
-     df_yx = drop_x_correlated(df_yx,run_cfg['preprocessing/x_corr_lb'])
-
-    #remove outliers
-    if run_cfg['preprocessing/outlier_removal']:
-     df_yx = remove_outlier(df_yx,run_cfg['preprocessing/outlier_cont_lim'])
+     df_x = drop_x_correlated(df_x,run_cfg['preprocessing/x_corr_lb'])
 
     #apply pca (with min max normalization)
     if run_cfg['preprocessing/apply_pca']:
