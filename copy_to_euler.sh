978e2248bcf4911caf9656ecafcc27d2fcc25f6a
ines <pereira.inez@gmail.com>
2020-11-05 19:34:51 +0100

    Also added statements to copy over to Euler

--- copy_to_euler.sh
+++ copy_to_euler.sh
@@ -2,8 +2,10 @@
 
 USERNAME=inesb # Change this to your username
 PROJECT=project2 # Change to project folder you want to copy
-AML_EULER=aml2020
+AML_CLUSTER=aml2020
 
-scp * $USERNAME@euler.ethz.ch:$AML_EULER
+# scp * $USERNAME@euler.ethz.ch:$AML_CLUSTER
 # Copying to Euler but excluding pickle and joblib files (othwerwise takes forever)
-rsync -P -r env modules $PROJECT --exclude={'*.pkl','*joblib','.git'} $USERNAME@euler.ethz.ch:$AML_EULER/.
+# rsync -P -r env modules $PROJECT --exclude={'*.pkl','*joblib','.git'} $USERNAME@euler.ethz.ch:$AML_CLUSTER/.
+scp * $USERNAME@login.leonhard.ethz.ch:$AML_CLUSTER
+rsync -P -r env modules $PROJECT --exclude={'*.pkl','*joblib','.git'} $USERNAME@login.leonhard.ethz.ch:$AML_CLUSTER/.
