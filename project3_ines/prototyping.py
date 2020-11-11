0f6762bdc1fb0fa910141b29865e0de2e7a07b1c
ines <pereira.inez@gmail.com>
2020-11-11 20:53:53 +0100

    Added to do's and references to look for

--- project3_ines/prototyping.py
+++ project3_ines/prototyping.py
@@ -32,16 +32,43 @@ class2 = y.index[y['y'] == 2].tolist()
 class3 = y.index[y['y'] == 3].tolist()
 
 # %%
-print(class1)
-print(class3)
+print(class2)
 
 # %% Observations:
 # - a lot of NaNs: but we probably need to extract features anyway
 # - Class imbalance
 
 # %% Plot some time series
-n = 25
+n = 2
 X.iloc[n,:].plot()
 plt.show()
 print(f'The corresponding class is: {y.iloc[n]}')
-# %%
+# %% Plan: define the features you want to look at.
+# 1. Preprocessing of data
+#    1.1. (Ines) Removal of low frequency components. R peaks should be at the same height
+#         + Smoothing of signal
+#    1.3. (Raffi) Artefact removal (outlier removal): visuelle Überprüfung
+#    1.4. (Francesco) Identify QRS complex and verify polarity
+#         + Find features related to waves
+# For an example: https://www.youtube.com/watch?v=WyjGCEWU4zY
+
+# (Raffi) To isolate class 3: 
+#    - Extract Fourier transform and plot mean for each class. You should see a peak
+#    - Number of artefacts
+# Detect inverted QRS and reinvert them?
+
+# Data-driven: convolutional nets? A mean wave with the variance?
+
+# References and software to check out:
+# From AML TA: https://biosppy.readthedocs.io/en/stable/biosppy.signals.html#biosppy-signals-ecg
+# Watch (general): https://www.youtube.com/watch?v=WyjGCEWU4zY
+# https://pypi.org/project/py-ecg-detectors/
+# Plotting: https://pypi.org/project/ecg-plot/
+# https://docs.scipy.org/doc/scipy/reference/generated/scipy.misc.electrocardiogram.html
+# Time series anomalies: https://www.youtube.com/watch?v=qN3n0TM4Jno
+# MATLAB code? https://www.youtube.com/watch?v=ofDHz77hm1c
+# In MATLAB File exchange: https://ch.mathworks.com/matlabcentral/fileexchange/74537-ecg-analyser?s_tid=srchtitle
+# Other in MATLAB FIle exchange: https://ch.mathworks.com/search.html?c%5B%5D=entire_site&q=ecg&page=1
+
+# Other ideas: consider nested CV https://www.youtube.com/watch?v=DuDtXtKNpZs
+
