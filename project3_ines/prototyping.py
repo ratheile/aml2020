71c515123b1fc459096d53b7d352eef589974053
ines <pereira.inez@gmail.com>
2020-11-14 15:07:08 +0100

    Added preprocessing from various libraries

--- project3_ines/prototyping.py
+++ project3_ines/prototyping.py
@@ -3,6 +3,11 @@ import pandas as pd
 import numpy as np
 import logging
 import matplotlib.pyplot as plt
+import peakutils # for basic peak-finding utils
+import pywt # for wavelet transform bits
+import wfdb # for physionet tools
+import biosppy
+import neurokit2 as nk
 
 import os
 repopath = '/Users/inespereira/Documents/Github/aml2020'
@@ -18,9 +23,9 @@ logging.info('I have imported your training dataset! :D')
 print(f'Shape of training set is {X.shape}')
 
 # %%
-print(X)
+# print(X)
 X.describe()
-print(y)
+# print(y)
 y.describe()
 
 # %% Making class imbalance apparent
@@ -43,6 +48,49 @@ n = 2
 X.iloc[n,:].plot()
 plt.show()
 print(f'The corresponding class is: {y.iloc[n]}')
+print(X.iloc[n,:])
+
+#%%
+ecg = X.iloc[n,:].dropna().to_numpy()
+plt.plot(ecg)
+plt.show()
+type(ecg)
+print(ecg)
+
+#%% Biosppy
+ecg_biosppy = biosppy.signals.ecg.ecg(
+  signal=ecg, 
+  sampling_rate=300, 
+  show=True)
+
+#%% Analyse biosppy summary
+# type(ecg_biosppy)
+# ecg_biosppy['ts']
+# ecg_biosppy['rpeaks']
+plt.plot(ecg_biosppy['filtered'])
+plt.show()
+
+# ecg_biosppy['templates_ts']
+# ecg_biosppy['templates']
+# ecg_biosppy['heart_rate_ts']
+# ecg_biosppy['heart_rate']
+
+# %% Wavelets
+wavelets = pywt.wavedec(
+  data=ecg,
+  wavelet='db4', # from YouTube Video
+  level=5
+)
+
+#%% Neurokit: has a bug and needs correction in source code or downgrading of sklearn
+bio_features = nk.bio_process(
+  ecg=ecg, 
+  sampling_rate=300,
+  ecg_filter_type='FIR',
+  ecg_filter_band='bandpass',
+  ecg_filter_frequency=[0.1,100],
+  ecg_segmenter='hamilton',
+)
 # %% Plan: define the features you want to look at.
 # 1. Preprocessing of data
 #    1.1. (Ines) Removal of low frequency components. R peaks should be at the same height
