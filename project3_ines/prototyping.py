2e4f41f90e140fd8e34f3c91838bba69ef980c48
ines <pereira.inez@gmail.com>
2020-11-14 19:22:05 +0100

    Added to do's and pipeline steps

--- project3_ines/prototyping.py
+++ project3_ines/prototyping.py
@@ -41,7 +41,7 @@ class2 = y.index[y['y'] == 2].tolist()
 class3 = y.index[y['y'] == 3].tolist()
 
 # %%
-print(class3)
+print(class2)
 
 # %% Observations:
 # - a lot of NaNs: but we probably need to extract features anyway
@@ -58,7 +58,7 @@ print(new_X_train)
 
 # %% Plot some time series
 # TODO: write for loop wrapper over this to apply preprocessing over all time series
-n = 10
+n = 3
 ecg = X.iloc[n,:].dropna().to_numpy()
 plt.plot(ecg)
 plt.show()
@@ -145,22 +145,33 @@ signal, waves = nk.ecg_delineate(ecg_signal, rpeaks, sampling_rate=sampling_rate
 # Data-driven: convolutional nets? A mean wave with the variance?
 
 # References and software to check out:
-# From AML TA: https://biosppy.readthedocs.io/en/stable/biosppy.signals.html#biosppy-signals-ecg
 # Watch (general): https://www.youtube.com/watch?v=WyjGCEWU4zY
 # https://pypi.org/project/py-ecg-detectors/
 # Plotting: https://pypi.org/project/ecg-plot/
 # https://docs.scipy.org/doc/scipy/reference/generated/scipy.misc.electrocardiogram.html
 # Time series anomalies: https://www.youtube.com/watch?v=qN3n0TM4Jno
-# MATLAB code? https://www.youtube.com/watch?v=ofDHz77hm1c
-# In MATLAB File exchange: https://ch.mathworks.com/matlabcentral/fileexchange/74537-ecg-analyser?s_tid=srchtitle
-# Other in MATLAB FIle exchange: https://ch.mathworks.com/search.html?c%5B%5D=entire_site&q=ecg&page=1
-
 # Other ideas: consider nested CV https://www.youtube.com/watch?v=DuDtXtKNpZs
 
-# Pipeline steps
-# 1. Detection and exclusion of class 3 from training set (TODO)
-# 2. Detection of flipped signals and flipping (TODO)
+# Preprocessing pipeline steps
+# 1. Detection and exclusion of class 3 from training set (TODO Raffi)
+# 2. Detection of flipped signals and flipping (TODO Raffi + Inês)
 # 3. Filtering (getting isoelectric line and smoothing)
 # 4. Waveform detection
-#   4.1 R-peaks and HR: mean_HR, std_HR
-#   4.2 P, QRS and T: number of P waves, mean_QRS_amplitude, mean_QRS_duration, std_QRS_duration (TODO: analysis of analysis results to get these features)
+#   4.1 R-peaks and HR: 
+#         - mean_HR (class 1)
+#         - std_HR (class 1)
+#   4.2 P, QRS and T: TODO (Francesco)
+#         - number of P waves, amplitude_P_wave (class 1)
+#         - mean_S_amplitude, ?? std_S_amplitude (class 2)
+#         - mean_QRS_duration, (class 2)
+#         - std_QRS_duration (class 2)
+
+# Build preprocessing pipeline TODO (Inês)
+#    - Remove preprocessing from last project (PCA, outlier detection, standardization)
+#    - Write down new Estimator
+#
+# Diagnostics plots: TODO (Raffi + Francesco): interactive, that show full time series and segmented P-QRS-T waves
+# import plotly.express as px
+#
+# Sanity checks
+#    - Check that HR and R-peaks between biosppy and Neurokit
