e1f94226368efb65c90d25d60a4db09884caf591
ines <pereira.inez@gmail.com>
2020-11-18 23:25:33 +0100

    Some corrections, removal of redundancy, make stuff prettier

--- project3_ines/feat_extr_ecg.py
+++ project3_ines/feat_extr_ecg.py
@@ -1,24 +1,33 @@
 #%% Imports
+# from .modules import ConfigLoader
 import pandas as pd
 import numpy as np
 import logging
 import matplotlib.pyplot as plt
 import seaborn as sns
-
 import os
-repopath = '/Users/francescofusaro/Documents/Github/aml2020'
-os.chdir(repopath)
-
 from biosppy.signals import ecg
 #from ecgdetectors import Detectors
 #from hrv import HRV
 import neurokit2 as nk
 
+# Load configs
+# env_cfg = ConfigLoader().from_file('env/env.yml')
+
+#%% Populate container for plot signals
+def populate_PlotData(PD,i,sample_id,class_id,raw_ecg,rpeaks_biosppy,filtered_biosppy,signals_neurokit):
+    PD[i][0]=sample_id
+    PD[i][1]=class_id
+    PD[i][2]=raw_ecg
+    PD[i][3]=rpeaks_biosppy
+    PD[i][4]=filtered_biosppy
+    PD[i][5]=signals_neurokit
+
 #%%Load Data Set
 def load_data(repopath):
-    X = pd.read_csv(f'{repopath}/project3_ffu/X_train_small.csv')
-    y = pd.read_csv(f'{repopath}/project3_ffu/y_train_small.csv')
-    #X_test = pd.read_csv(f'{repopath}/project3_ffu/X_test.csv')
+    X = pd.read_csv(os.path.join(repopath, 'project3_ines', 'X_train_small.csv'))
+    y = pd.read_csv(os.path.join(repopath, 'project3_ines', 'y_train_small.csv'))
+    #X_test = pd.read_csv(os.path.join(env_cfg["datasets/project2/path"],'X_test.csv'))
     X_test = 0
     logging.info('Dataset imported')
     
@@ -32,25 +41,18 @@ def split_classes(X,y):
     class3_ls = y.index[y['y'] == 3].tolist() #Noise
     
     X0 = X.iloc[class0_ls,:]
-    df_X0 = pd.DataFrame(data=X0,columns=X.columns)
-    
     X1 = X.iloc[class1_ls,:]
-    df_X1 = pd.DataFrame(data=X1,columns=X.columns)
-    
     X2 = X.iloc[class2_ls,:]
-    df_X2 = pd.DataFrame(data=X2,columns=X.columns)
-    
     X3 = X.iloc[class3_ls,:]
-    df_X3 = pd.DataFrame(data=X3,columns=X.columns)
     
-    return(df_X0, df_X1, df_X2, df_X3)
+    return(X0, X1, X2, X3)
 
 ##% Define more flexible ecg_process function
-def ecg_process_AML(ecg_signal, sampling_rate=1000, method="neurokit"):
+def ecg_process_AML(ecg_signal, sampling_rate=300, method="neurokit"):
     """Process an ECG signal as original neurokit2 function, see:
     https://neurokit2.readthedocs.io/en/latest/_modules/neurokit2/ecg/ecg_process.html#ecg_process
     
-    However to increase flexibility, 'method' parameter is specifically set for each subfuntion call:
+    However, to increase flexibility, 'method' parameter is specifically set for each subfunction call:
     
     - ecg_clean methods: Can be one of ‘neurokit’ (default), ‘biosppy’, ‘pamtompkins1985’, ‘hamilton2002’, ‘elgendi2010’, ‘engzeemod2012’.
     
@@ -60,58 +62,82 @@ def ecg_process_AML(ecg_signal, sampling_rate=1000, method="neurokit"):
     see: https://neurokit2.readthedocs.io/en/latest/examples/ecg_delineate.html
     """
     
-    #clean
-    ecg_preprocess_clean_method = 'biosppy' 
-    ecg_cleaned = nk.ecg.ecg_clean(ecg_signal, sampling_rate=sampling_rate, method=ecg_preprocess_clean_method)
-    # R-peaks
-    ecg_preprocess_rpeaks_method = 'neurokit'
-    instant_peaks, rpeaks, = nk.ecg.ecg_peaks(
-        ecg_cleaned=ecg_cleaned, sampling_rate=sampling_rate, method=ecg_preprocess_rpeaks_method, correct_artifacts=True
+    # Filtering and smoothing
+    ecg_preprocess_clean_method = 'biosppy' # TODO: from base_cfg
+    ecg_cleaned = nk.ecg.ecg_clean(
+        ecg_signal,
+        sampling_rate=sampling_rate,
+        method=ecg_preprocess_clean_method
+        )
+    
+    # R-peak detection
+    # Following example from: https://neurokit2.readthedocs.io/en/latest/functions.html#neurokit2.ecg.ecg_rsp
+    ecg_preprocess_rpeaks_method = 'neurokit' # TODO: from base_cfg
+    rpeaks, rpeaks_info = nk.ecg.ecg_peaks(
+        ecg_cleaned=ecg_cleaned,
+        sampling_rate=sampling_rate,
+        method=ecg_preprocess_rpeaks_method,
+        correct_artifacts=True
     )
 
-    rate = nk.signal_rate(rpeaks, sampling_rate=sampling_rate, desired_length=len(ecg_cleaned))
+    rate = nk.signal_rate(
+        rpeaks,
+        sampling_rate=sampling_rate,
+        desired_length=len(ecg_cleaned)
+        )
 
+    # Quality evaluation: https://neurokit2.readthedocs.io/en/latest/functions.html#neurokit2.ecg.ecg_quality
     quality = nk.ecg.ecg_quality(ecg_cleaned, rpeaks=None, sampling_rate=sampling_rate)
 
+    # Create new dataframe with raw and clean ECG, as well as rate and quality
     signals = pd.DataFrame({"ECG_Raw": ecg_signal, "ECG_Clean": ecg_cleaned, "ECG_Rate": rate, "ECG_Quality": quality})
 
     # Additional info of the ecg signal
     ecg_preprocess_delineate_method = 'dwt'
     delineate_signal, delineate_info = nk.ecg.ecg_delineate(
-        ecg_cleaned=ecg_cleaned, rpeaks=rpeaks, sampling_rate=sampling_rate, method=ecg_preprocess_delineate_method
+        ecg_cleaned=ecg_cleaned,
+        rpeaks=rpeaks_info,
+        sampling_rate=sampling_rate,
+        method=ecg_preprocess_delineate_method
     )
     
     if ecg_preprocess_delineate_method != 'peak':
         # 'dwt' and 'cwt' Unlike the peak method, 'dwt' and 'cwt' does not idenfity the Q-peaks and S-peaks.
         delineate_signal_peak, delineate_info = nk.ecg.ecg_delineate(
-        ecg_cleaned=ecg_cleaned, rpeaks=rpeaks, sampling_rate=sampling_rate, method='peak' 
+        ecg_cleaned=ecg_cleaned, 
+        rpeaks=rpeaks_info, 
+        sampling_rate=sampling_rate, 
+        method='peak'
         )
         delineate_signal['ECG_Q_Peaks'] = delineate_signal_peak['ECG_Q_Peaks']
         delineate_signal['ECG_S_Peaks'] = delineate_signal_peak['ECG_S_Peaks']
         
-    cardiac_phase = nk.ecg.ecg_phase(ecg_cleaned=ecg_cleaned, rpeaks=rpeaks, delineate_info=delineate_info)
+    cardiac_phase = nk.ecg.ecg_phase(
+        ecg_cleaned=ecg_cleaned,
+        rpeaks=rpeaks_info,
+        delineate_info=delineate_info
+        )
+    # Reconstructing ouput identical to nk.ecg_process We need this to use nk.ecg_analyze
+    signals = pd.concat([signals, rpeaks, delineate_signal, cardiac_phase], axis=1)
 
-    signals = pd.concat([signals, instant_peaks, delineate_signal, cardiac_phase], axis=1)
-
-    info = rpeaks
-    return signals, info
+    return signals, rpeaks_info
       
 #%% Extracted peaks summary
 def calc_peak_summary(signals, sampling_rate):
-    #peak summary
+    # Peak summary
     summary = []
+    sig_pp = signals[signals['ECG_P_Peaks'] == 1]
+    p_count= len(sig_pp)
     sig_qq = signals[signals['ECG_Q_Peaks'] == 1]
     q_count = len(sig_qq)
     sig_rr = signals[signals['ECG_R_Peaks'] == 1]
     r_count = len(sig_rr)
-    sig_pp = signals[signals['ECG_P_Peaks'] == 1]
-    p_count= len(sig_pp)
     sig_ss = signals[signals['ECG_S_Peaks'] == 1]
     s_count = len(sig_ss)
     sig_tt = signals[signals['ECG_T_Peaks'] == 1]
     t_count = len(sig_tt)
     
-    #peak counts
+    # Peak counts
     p_rel = p_count/r_count
     q_rel = q_count/r_count
     s_rel = s_count/r_count
@@ -122,19 +148,19 @@ def calc_peak_summary(signals, sampling_rate):
     summary.append(s_rel)
     summary.append(t_rel)
     
-    #peak p amplitude
+    # Peak p amplitude
     p_mean = sig_pp['ECG_Clean'].mean()
     summary.append(p_mean)
     p_std = sig_pp['ECG_Clean'].std()
     summary.append(p_std)
     
-    #peak s amplitude
+    # Peak s amplitude
     s_mean = sig_ss['ECG_Clean'].mean()
     summary.append(s_mean)
     s_std = sig_ss['ECG_Clean'].std()
     summary.append(s_std)
     
-    #QRS duration
+    # QRS duration
     sig_r_onset = signals[signals['ECG_R_Onsets'] == 1]
     sig_r_offset = signals[signals['ECG_R_Offsets'] == 1]
     if (len(sig_r_onset) == len(sig_r_offset)):
@@ -154,23 +180,34 @@ def calc_peak_summary(signals, sampling_rate):
     return summary
 
 #%% extract features from ECGs
-def extract_features(df, Fs, feature_list, remove_outlier, biosppy_enabled, ecg_quality_check, ecg_quality_threshold, class_id):
+def extract_features(df, Fs, feature_list, remove_outlier, biosppy_enabled, ecg_quality_check, ecg_quality_threshold, class_id, verbose):
     
     if remove_outlier:
         logging.info('Removing ecg outliers with pyheart...')
         
     if biosppy_enabled:
-        logging.info('Pre-filtering ECG with biosspy')
+        logging.info('Pre-filtering with biosspy activated')
     
     # Define F array to aggregate extracted sample features
     F=np.zeros([df.shape[0],len(feature_list)])
     
+    # Define PD as a list array to aggregate extracted sample infos (for later plotting)
+    # PD columns: [0:sample id | 1: class id | 2: raw signal| 3: r_peaks_biosspy | 4: filtered biosppy | 5: signals neurokit ]
+    # PD rows: number of ecg signals
+    plotData = []
+    for n_row in range(df.shape[0]):
+        column = []
+        for n_col in range(6):
+            column.append(0)
+            plotData.append(column)
+    
     # for all the rows in the df
     for i in range(len(df)):
-        sig_i = df.iloc[i,1:] #signal i wo sample id
-        sig_i = sig_i.replace(to_replace='NaN',value=np.nan)
-        sig_i_np = (sig_i.to_numpy()).ravel()
-        sig_i_np = sig_i_np[~np.isnan(sig_i_np)]
+        # sig_i = df.iloc[i,1:] #signal i without sample id
+        # sig_i = sig_i.replace(to_replace='NaN',value=np.nan)
+        # sig_i_np = (sig_i.to_numpy()).ravel()
+        # sig_i_np = sig_i_np[~np.isnan(sig_i_np)] #this is our ecg raw signal
+        sig_i_np = df.iloc[i, 1:].dropna().to_numpy()
         
         # remove outliers using pyheart?
         if remove_outlier:
@@ -192,18 +229,21 @@ def extract_features(df, Fs, feature_list, remove_outlier, biosppy_enabled, ecg_
 
                 (ts, filtered_biosppy, rpeaks_biosppy, templates_ts, 
                 templates, heart_rate_ts, heart_rate) = out
+                
+                no_rpeaks_biosppy = len(rpeaks_biosppy)
         
             except Exception:
                 logging.info(f'biosppy crashed for sample {i} in class {class_id}')
                 rpeaks_biosppy = np.nan
+                no_rpeaks_biosppy = np.nan
                 filtered_biosppy = np.nan
         else:
             rpeaks_biosppy = np.nan
+            no_rpeaks_biosppy = np.nan
             filtered_biosppy = np.nan
+
             
-        # process ecg sample with with neurokit
-        # signals, info = nk.ecg_process(sig_i_np, sampling_rate=Fs)
-        # use customized function
+        # Preprocessing of ECG time series with Neurokit2 using customized function
         try: 
             signals, info = ecg_process_AML(sig_i_np, sampling_rate=Fs)
             
@@ -225,8 +265,8 @@ def extract_features(df, Fs, feature_list, remove_outlier, biosppy_enabled, ecg_
             feat_i = [df.iloc[i,0]] # init a list with sample id
             feat_i.append(ecg_q_mean)
             feat_i.append(ecg_q_std,)
-            feat_i.append(df_analyze.iloc[0,0]) #ECG_Rate_Mean
-            feat_i.append(df_analyze.iloc[0,1]) #HRV_RMSSD
+            feat_i.append(df_analyze.loc[0,'ECG_Rate_Mean'])
+            feat_i.append(df_analyze.loc[0,'HRV_RMSSD'])
             feat_i.append(len(rpeaks_biosppy)) #no. of detected r-peaks in biosspy
             for elem in peak_summary_neurokit:
                 feat_i.append(elem)
@@ -235,21 +275,25 @@ def extract_features(df, Fs, feature_list, remove_outlier, biosppy_enabled, ecg_
             n = len(feature_list)
             feat_i = [np.nan]*n
             feat_i[0] = df.iloc[i,0] # sample id
-            feat_i[5] = len(rpeaks_biosppy) #maybe biosppy worked
+            feat_i[5] = no_rpeaks_biosppy #maybe biosppy worked
         
         F[i,:] = feat_i
-        
+        plotData = populate_PlotData(plotData,i,df.iloc[i,0],class_id,sig_i_np,rpeaks_biosppy,filtered_biosppy,signals)
+        if verbose:
+            sample_left = df.shape[0]-i
+            print(f'Preprocessed ECG sample {i}({df.iloc[i,0]}) in class {class_id}... {sample_left} samples to go!')
         #TODO: in a suitable container collect the sample id and the signals dataframe (output of neurokit), which
         #which contains all the info for the plots
     
     feat_df = pd.DataFrame(data=F,columns=feature_list)
     
-    return(feat_df)    
+    return(feat_df, plotData)    
      
 #%% Main
 
-repopath = '/Users/francescofusaro/Documents/Github/aml2020'
+repopath = '/Users/inespereira/Documents/Github/aml2020'
 os.chdir(repopath)
+SAMPLING_RATE = 300
 
 #%% Load data from repo (keep sample id for later use)
 X, y, X_test = load_data(repopath)
@@ -267,35 +311,41 @@ feature_list = ['Sample_Id',
 
 
 #%% Feature extraction class 0
-X0_features = extract_features(df=X0,
-                               Fs = 300,
+X0_features, X0_plotData = extract_features(df=X0,
+                               Fs = SAMPLING_RATE,
                                feature_list = feature_list, 
                                remove_outlier=True, 
                                biosppy_enabled=True, 
                                ecg_quality_check=True, 
                                ecg_quality_threshold=0.8, 
-                               class_id=0)
+                               class_id=0,
+                               verbose=True
+                               )
 
 X0_features.head()
 #%% Feature extraction class 1
-X1_features = extract_features(df=X1,
-                               Fs = 300,
+X1_features, X1_plotData = extract_features(df=X1,
+                               Fs = SAMPLING_RATE,
                                feature_list = feature_list, 
                                remove_outlier=False, 
-                               biosppy_enabled=False, 
+                               biosppy_enabled=True, 
                                ecg_quality_check=False, 
                                ecg_quality_threshold=0.8, 
-                               class_id=1)
+                               class_id=1,
+                               verbose=True
+                               )
 X1_features.head()
 #%% Feature extraction class 2
-X2_features = extract_features(df=X2,
-                               Fs = 300,
+X2_features, X2_plotData = extract_features(df=X2,
+                               Fs = SAMPLING_RATE,
                                feature_list = feature_list, 
                                remove_outlier=True, 
                                biosppy_enabled=True, 
                                ecg_quality_check=True, 
                                ecg_quality_threshold=0.8, 
-                               class_id=2)
+                               class_id=2,
+                               verbose=True
+                               )
 X2_features.head()
 #%% Write pickle or similar
 #TODO: write to pickle or similar for the features dataframes and ecg prepocessing
