4b0b378aeec912e7bce7dd1de514c48e933d53f4
frafusaro <fra.fusaro@gmail.com>
2020-11-22 11:12:21 +0100

    added signal flipping

--- project3/feature_extraction.py
+++ project3/feature_extraction.py
@@ -112,7 +112,8 @@ def ecg_process_AML(ecg_signal, sampling_rate=300):
   return signals, rpeaks_info
       
 # Extracted peaks summary
-def calc_peak_summary(signals, sampling_rate):
+def calc_peak_summary(is_flipped, signals, sampling_rate):
+  
   # Peak summary
   summary = []
   sig_pp = signals[signals['ECG_P_Peaks'] == 1]
@@ -149,24 +150,32 @@ def calc_peak_summary(signals, sampling_rate):
   s_std = sig_ss['ECG_Clean'].std()
   summary.append(s_std)
   
+   #check whether the signal is flipped
+  if not is_flipped and (
+    abs(sig_qq['ECG_Clean'].mean()) > abs(sig_rr['ECG_Clean'].mean()) or
+    abs(sig_ss['ECG_Clean'].mean()) > abs(sig_rr['ECG_Clean'].mean())
+  ):
+    is_flipped = True
+    return summary, is_flipped #exit the function and try again with flipped signal
+  
   # QRS duration
   sig_r_onset = signals[signals['ECG_R_Onsets'] == 1]
   sig_r_offset = signals[signals['ECG_R_Offsets'] == 1]
   if (len(sig_r_onset) == len(sig_r_offset)):
-      d_qrs_N = sig_r_offset.index.to_numpy().ravel() - sig_r_onset.index.to_numpy().ravel() #number of samples between R Onset and Offset
-      d_qrs_t = d_qrs_N / sampling_rate
-      d_qrs_t_mean = d_qrs_t.mean()
-      d_qrs_t_std = d_qrs_t.std()
+    d_qrs_N = sig_r_offset.index.to_numpy().ravel() - sig_r_onset.index.to_numpy().ravel() #number of samples between R Onset and Offset
+    d_qrs_t = d_qrs_N / sampling_rate
+    d_qrs_t_mean = d_qrs_t.mean()
+    d_qrs_t_std = d_qrs_t.std()
   else:
-      #TODO: in case of unenven R Onset and Offset detection develop more sofisticated algo to check which peaks can be retained?
-      d_qrs_t_mean = np.nan
-      d_qrs_t_std = np.nan
+    #TODO: in case of unenven R Onset and Offset detection develop more sofisticated algo to check which peaks can be retained?
+    d_qrs_t_mean = np.nan
+    d_qrs_t_std = np.nan
       
   
   summary.append(d_qrs_t_mean)
   summary.append(d_qrs_t_std)
   
-  return summary
+  return summary, is_flipped
 
 # Extract features from ECGs
 def extract_features(run_cfg, df, feature_list, y=None, verbose=False):
@@ -241,6 +250,8 @@ def extract_features(run_cfg, df, feature_list, y=None, verbose=False):
 
           
       # Preprocessing of ECG time series with Neurokit2 using customized function
+      nk2_crash_after_sig_flip = False
+      is_flipped = False
       try: 
         signals, info = ecg_process_AML(sig_i_np, sampling_rate=Fs)
         
@@ -248,24 +259,60 @@ def extract_features(run_cfg, df, feature_list, y=None, verbose=False):
             #TODO: keep only the signals with ecq quality above threshold?
             dummy=1
         
-        # calculate ecg signal HR indicators
-        df_analyze = nk.ecg_analyze(signals, sampling_rate=Fs, method='auto')
-        
         # filter signals for peak counts, amplitudes, and QRS event duration
-        peak_summary_neurokit = calc_peak_summary(signals=signals, sampling_rate=Fs)
+        peak_summary_neurokit, is_flipped = calc_peak_summary(is_flipped, signals=signals, sampling_rate=Fs)
         
-        # calculate the mean and standard devation of the signal quality
-        ecg_q_mean = signals['ECG_Quality'].mean() 
-        ecg_q_std = signals['ECG_Quality'].std()
-        
-        # consolidate the features for sample i
-        feat_i = [class_id] # find sample ID based on index
-        feat_i.append(ecg_q_mean) # ECG_Quality_Mean
-        feat_i.append(ecg_q_std,) # ECG_Quality_STD
-        feat_i.append(df_analyze.loc[0,'ECG_Rate_Mean']) # ECG_Rate_Mean
-        feat_i.append(df_analyze.loc[0,'HRV_RMSSD']) # ECG_HRV
-        feat_i.append(len(rpeaks_biosppy)) #no. of detected r-peaks in biosspy (R_P_biosppy)
-        feat_i.extend(peak_summary_neurokit) # extend() does inplace update to the original list
+        #repeat feature extraction with flipped signal
+        if is_flipped:
+          logging.info(f'swapped signal detected: mirroring sample {i} in class {class_id}')
+          
+          #mirror the signal
+          sig_i_np = -sig_i_np
+          
+          #biosppy
+          rpeaks_biosppy = np.nan
+          no_rpeaks_biosppy = np.nan
+          filtered_biosppy = np.nan
+          if biosppy_enabled:
+            try:
+              out = ecg.ecg(signal=sig_i_np, sampling_rate=Fs, show=False)
+              
+              (ts, filtered_biosppy, rpeaks_biosppy, templates_ts, 
+              templates, heart_rate_ts, heart_rate) = out
+              no_rpeaks_biosppy = len(rpeaks_biosppy)
+            except Exception:
+              logging.info(f'biosppy crashed after flipping sample {i} in class {class_id}')
+          
+          #neurokit2
+          try:
+            signals, info = ecg_process_AML(sig_i_np, sampling_rate=Fs)
+            peak_summary_neurokit, is_flipped = calc_peak_summary(is_flipped, signals=signals, sampling_rate=Fs)
+          except Exception:
+            logging.info(f'neurokit crashed after flipping sample {i} in class {class_id}')
+            nk2_crash_after_sig_flip = True
+            
+            n = len(feature_list)
+            feat_i = [np.nan]*n
+            feat_i[0] = df.iloc[i,0] # sample id
+            feat_i[5] = no_rpeaks_biosppy #maybe biosppy worked
+            signals = np.nan
+      
+        # calculate ecg signal HR indicators
+        if not nk2_crash_after_sig_flip:
+          df_analyze = nk.ecg_analyze(signals, sampling_rate=Fs, method='auto')
+          
+          # calculate the mean and standard devation of the signal quality
+          ecg_q_mean = signals['ECG_Quality'].mean() 
+          ecg_q_std = signals['ECG_Quality'].std()
+          
+          # consolidate the features for sample i
+          feat_i = [class_id] # find sample ID based on index
+          feat_i.append(ecg_q_mean) # ECG_Quality_Mean
+          feat_i.append(ecg_q_std,) # ECG_Quality_STD
+          feat_i.append(df_analyze.loc[0,'ECG_Rate_Mean']) # ECG_Rate_Mean
+          feat_i.append(df_analyze.loc[0,'HRV_RMSSD']) # ECG_HRV
+          feat_i.append(len(rpeaks_biosppy)) #no. of detected r-peaks in biosspy (R_P_biosppy)
+          feat_i.extend(peak_summary_neurokit) # extend() does inplace update to the original list
       except Exception:
         logging.info(f'neurokit2 crashed for sample {i} in class {class_id}')
         n = len(feature_list)
