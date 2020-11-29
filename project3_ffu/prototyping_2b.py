268fcfa4d13e934c9d8a8de85cc5939592e096df
frafusaro <fra.fusaro@gmail.com>
2020-11-29 20:51:03 +0100

    chnages from meeting

--- project3_ffu/prototyping_2b.py
+++ project3_ffu/prototyping_2b.py
@@ -275,7 +275,41 @@ epoch_mask = np.zeros((crv.shape[0],show_n_epochs), dtype=np.bool_)
 
 for i in range(show_n_epochs):
   epoch_mask[heartbeats[str(i+1)]['Index'].values, i] = True
+
+#%% hearbeat mask for francesco
+rpeaks_0_1 = signals['ECG_R_Peaks']
+rpeaks_window_dict = {}
+for key, df in heartbeats.items():
+  epoch_mask = np.zeros((crv.shape[0]), dtype=np.bool_)
+  epoch_mask[df['Index'].values] = True
+  peak_ind = np.logical_and(epoch_mask, filter_mask)
+  peak_ind = np.logical_and(peak_ind, rpeaks_0_1)
+  where = np.where(peak_ind)[0]
+  if len(where) > 0:
+    assert len(where) == 1
+    rpeaks_window_dict[where[0]] = (key, df)
+
+#%% ###################### Extract Segment Lengths for one signal #################
+#apply filter mask to our signal
+sig_filtered= signals[filter_mask]
+killed_idx_ratio = 1-len(sig_filtered)/len(signals)
+#TODO: kill the sample if good to bad is below 10%?
+
+#extract indexes
+r_f = np.array(vec_to_ind(sig_filtered['ECG_R_Peaks']))
+p_ons_f = np.array(vec_to_ind(sig_filtered['ECG_P_Onsets']))
+p_f = np.array(vec_to_ind(sig_filtered['ECG_P_Peaks']))
+p_off_f = np.array(vec_to_ind(sig_filtered['ECG_P_Offsets']))
+r_ons_f = np.array(vec_to_ind(sig_filtered['ECG_R_Onsets']))
+q_f = np.array(vec_to_ind(sig_filtered['ECG_Q_Peaks']))
+s_f = np.array(vec_to_ind(sig_filtered['ECG_S_Peaks']))
+r_off_f = np.array(vec_to_ind(sig_filtered['ECG_R_Offsets']))
+t_ons_f = np.array(vec_to_ind(sig_filtered['ECG_T_Onsets']))
+t_f = np.array(vec_to_ind(sig_filtered['ECG_T_Peaks']))
+t_off_f = np.array(vec_to_ind(sig_filtered['ECG_T_Offsets']))
+
 #%%
+#loop through each epoch (r_peaks) and extract corresponding secondary peaks
 ###################### Extract Segment Lengths for one signal #################
 ###Preparations and apply filter mask
 #define array where to store the data
@@ -293,23 +327,16 @@ def find_nearest(array, value):
   idx = (np.abs(array - value)).argmin()
   return array[idx]
 
-def pick_a_peak(summary,idx,r_peak,sec_peaks_vec,to_the):
+def pick_a_peak(summary,idx,r_peak,sec_peaks_vec,to_the,window_bounds):
   
   #check for candidates
   if to_the == 'left':
-    sec_peaks_vec[np.greater(r_peak,sec_peaks_vec)]
-    if len(sec_peaks_vec) > 0:
-      #find the the nearest
-      print(f'I entered the left if for idx = {idx}')
-      print(f'sec_peaks_vec = {sec_peak_v}, r_peak = {r_peak}')
-      p_idx = find_nearest(sec_peaks_vec,r_peak)
-      summary[idx]=p_idx
+    v = sec_peaks_vec[np.logical_and(np.greater_equal(r_peak,sec_peaks_vec),np.less(window_bounds[0],sec_peaks_vec))]
   elif to_the == 'right':
-    sec_peaks_vec[~np.greater(r_peak,sec_peaks_vec)]
-    if len(sec_peaks_vec) > 0:
+    v = sec_peaks_vec[np.logical_and(np.less_equal(r_peak,sec_peaks_vec),np.greater(window_bounds[1],sec_peaks_vec))]
+
+  if len(v) > 0:
       #find the the nearest
-      print(f'I entered the right if for idx = {idx}')
-      print(f'sec_peaks_vec = {sec_peak_v}, r_peak = {r_peak}')
       p_idx = find_nearest(sec_peaks_vec,r_peak)
       summary[idx]=p_idx
     
@@ -342,35 +369,22 @@ def calc_distances(ep_i_p_idx):
   
   return distances
 
-#apply filter mask to our signal
-sig_filtered= signals[filter_mask]
-killed_idx_ratio = 1-len(sig_filtered)/len(signals)
-#TODO: kill the sample if good to bad is below 10%?
-
-#extract indexes
-r_f = np.array(vec_to_ind(sig_filtered['ECG_R_Peaks']))
-p_ons_f = np.array(vec_to_ind(sig_filtered['ECG_P_Onsets']))
-p_f = np.array(vec_to_ind(sig_filtered['ECG_P_Peaks']))
-p_off_f = np.array(vec_to_ind(sig_filtered['ECG_P_Offsets']))
-r_ons_f = np.array(vec_to_ind(sig_filtered['ECG_R_Onsets']))
-q_f = np.array(vec_to_ind(sig_filtered['ECG_Q_Peaks']))
-s_f = np.array(vec_to_ind(sig_filtered['ECG_S_Peaks']))
-r_off_f = np.array(vec_to_ind(sig_filtered['ECG_R_Offsets']))
-t_ons_f = np.array(vec_to_ind(sig_filtered['ECG_T_Onsets']))
-t_f = np.array(vec_to_ind(sig_filtered['ECG_T_Peaks']))
-t_off_f = np.array(vec_to_ind(sig_filtered['ECG_T_Offsets']))
-
-#%%
-#Preparations and apply filter mask
-#loop through each epoch (r_peaks) and extract corresponding secondary peaks
+def rpeaks_window_bounds(r_peak_id,rpeaks_window_dict,eps):
+    _, df_wind = rpeaks_window_dict[r_peak_id]
+    lb = df_wind.Index.min() - eps
+    ub = df_wind.Index.max() + eps
+    
+    return np.array([lb,ub])
+    
 E= np.zeros(shape=(len(r_f),55)) # r_peaks * 55 distances measure in each epoch
-for i in range(1):
+for i in range(len(r_f)):
   init_ep_i_idx, _ = init_arrays()
   ep_i_p_idx = init_ep_i_idx
+  window_bounds = rpeaks_window_bounds(r_f[i],rpeaks_window_dict,50)
   #to the left
   sec_peaks_left = [p_ons_f,p_f,p_off_f,r_ons_f,q_f]
   for idx, sec_peak_v in enumerate(sec_peaks_left):
-      ep_i_p_idx = pick_a_peak(ep_i_p_idx,idx,r_f[i],sec_peak_v,'left')
+      ep_i_p_idx = pick_a_peak(ep_i_p_idx,idx,r_f[i],sec_peak_v,'left', window_bounds)
   
   ep_i_p_idx[5]=r_f[i]
   
@@ -378,7 +392,7 @@ for i in range(1):
   sec_peaks_right = [s_f,r_off_f,t_ons_f,t_f,t_off_f]
   idx_offset = 6
   for idx, sec_peak_v in enumerate(sec_peaks_right):
-      ep_i_p_idx = pick_a_peak(ep_i_p_idx,idx+idx_offset,r_f[i],sec_peak_v,'right')
+      ep_i_p_idx = pick_a_peak(ep_i_p_idx,idx+idx_offset,r_f[i],sec_peak_v,'right',window_bounds)
         
   #TODO: count the nans and reject the epoch if too many are missing?
   #compute the distances
